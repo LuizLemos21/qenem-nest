f992e7b938578084d1a7a539015290f3c02445f0
LuizLemos21 <104809823+LuizLemos21@users.noreply.github.com>
2024-06-27 23:37:22 -0300

    api running

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { SimulationModule } from './modules/simulation.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '',
       database: 'questenem',
       autoLoadEntities: true,
       synchronize: true,
